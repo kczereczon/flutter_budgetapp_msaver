ee63589f52d2aede3681e189e51a6cdd8e14b167
Krzysztof <kczereczon@gmail.com>
2020-12-17 21:02:51 +0100

    Przerobienie widoku, tak aby wyświetlały się wydatki

--- lib/screens/home/components/Body.dart
+++ lib/screens/home/components/Body.dart
@@ -8,14 +8,73 @@ class Body extends StatelessWidget {
         Padding(
             child: Buttons(),
             padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20)),
-        Container(
-          padding: EdgeInsets.all(15),
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(10)),
-          width: MediaQuery.of(context).size.width * 0.9,
-          height: 70,
+        Column(
+          children: [
+            Outcome(),
+            Outcome(),
+            Outcome(),
+            Outcome(),
+            Outcome(),
+            Outcome(),
+            Outcome(),
+            Outcome(),
+            Outcome(),
+            Outcome(),
+          ],
         )
       ],
+    );
+  }
+}
+
+class Outcome extends StatelessWidget {
+  const Outcome({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 8),
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(10)),
+      width: MediaQuery.of(context).size.width * 0.9,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Opłata za mieszkanie"),
+              Text(
+                "1800zł",
+                style: TextStyle(fontWeight: FontWeight.w300),
+              )
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("wczoraj", style: TextStyle(fontWeight: FontWeight.w200)),
+              Container(
+                child: Text("Mieszkanie",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    )),
+                padding: EdgeInsets.symmetric(horizontal: 13.0),
+                decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(10)),
+              )
+            ],
+          ),
+        ],
+      ),
+      height: 70,
     );
   }
 }
@@ -25,8 +84,7 @@ class Buttons extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
         height: 30,
-        child:
-         Row(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             RaisedButton(
@@ -34,7 +92,7 @@ class Buttons extends StatelessWidget {
               onPressed: () => null,
               color: Color.lerp(Colors.transparent, Colors.grey[100], 0.6),
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+                borderRadius: BorderRadius.circular(10.0),
               ),
             ),
             RaisedButton(
@@ -54,7 +112,6 @@ class Buttons extends StatelessWidget {
               ),
             ),
           ],
-        )
-    );
+        ));
   }
 }
